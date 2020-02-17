908a2b11c147c2b0b3adaf74229831556c634430
goldenPiGames <goldenpigames@gmail.com>
2020-02-17 11:33:08 -0500

    things don't vanish in mobile

--- src/UIObjects/UIObjectBase.js
+++ src/UIObjects/UIObjectBase.js
@@ -23,9 +23,9 @@ class UIObject {
 		this.clicked = mouse.clicked && this.hovered;
 		this.released = this.held && !mouse.down;
 		this.held = (this.clicked || (this.held && mouse.down)) ? this.held+1 : 0;
-		this.draggedX = this.held && mouse.x - mouse.lastX;
-		this.draggedY = this.held && mouse.y - mouse.lastY;
-		this.draggedTheta = this.held && (Math.atan2(mouse.y-this.y, mouse.x-this.x) - Math.atan2(mouse.lastY-this.y, mouse.lastX-this.x));
+		this.draggedX = this.held && mouse.x - mouse.lastX || 0;
+		this.draggedY = this.held && mouse.y - mouse.lastY || 0;
+		this.draggedTheta = this.held && (Math.atan2(mouse.y-this.y, mouse.x-this.x) - Math.atan2(mouse.lastY-this.y, mouse.lastX-this.x)) || 0;
 	}
 }
 
